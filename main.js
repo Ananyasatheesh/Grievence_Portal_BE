5b9e4cc266b6df5661ae86ab66c02cd619f57dec
Andrew <andrewgithub010@gmail.com>
2025-09-06 19:24:39 +0530

    Andrew | Added endpoints for raising and otp operations

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectDB = require("./db/connect");
+const grievance = require('./routes/grievance/index')
+const otp = require("./routes/otp/index")
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/grievance", grievance);
+app.use("/otp", otp);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ models/grievanceSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const grievanceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    grievance: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Grievance", grievanceSchema);
